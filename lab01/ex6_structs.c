72120e81ef12e9990e2ea77ba7c2ff2f5e90c0fc
Jeffrey Lin <jeffreylin888888@berkeley.edu>
2025-09-10 23:21:20 -0700

    lab01: finish ex6 (structs)

--- lab01/ex6_structs.c
+++ lab01/ex6_structs.c
@@ -1,10 +1,30 @@
 #include <stdio.h>
-#include <string.h>
-struct Student { int id; char name[32]; };
+#include <stdlib.h>
+
+// Structs allow you to hold data items of different types in a single variable
+// Struct definitions can be used to declare a struct variable within your program
+// Struct definitions are typically done outside of a function
+struct Student {
+    int id;
+    char* name;
+};
+
 int main() {
-  struct Student s; s.id = 61; strcpy(s.name, "Ada");
-  printf("Student id: %d, name: %s\n", s.id, s.name);
-  struct Student *sp = &s; sp->id += 1; strcpy(sp->name, "Grace");
-  printf("Student id: %d, name: %s\n", sp->id, sp->name);
+  // TODO: declare a variable student of type struct Student
+  // Note: this struct is stored on the stack
+  struct Student student;
+
+  // TODO: print out the size of a struct Student
+  // While this may seem out of place now, it will be useful in the future!
+  // Hint: there's an operator that can calculate this for you!
+  printf("Size of a struct Student: %lu bytes\n", (unsigned long)sizeof(struct Student));
+
+  // TODO: set student's id field to 5
+  // Hint: the dot notation accesses a struct's fields
+  student.id = 5;
+
+  // TODO: print out student's id field
+  printf("Student's ID: %d\n", student.id);
+
   return 0;
 }
